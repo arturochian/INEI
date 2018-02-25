eb1a6fb1cb145adc50afd9d2ed6320f363f65f07
arturochian <arturob.chian@gmail.com>
2018-02-25 14:17:39 -0500

    Data lista!

---
+++ R/INEI.R
@@ -0,0 +1,110 @@
+#' El paquete para trabajar con data de INEI. Creado por BEST
+#'
+#' @name INEI
+#' @docType package
+#' @import zoo
+#' @import xts
+#' @import dplyr
+
+NULL
+
+#' Base de datos de Hogares con acceso a agua potable por Red Publica.
+#'
+#' Base de datos de Hogares con acceso a agua potable por Red Publica del Sistema de Informacion Regional para la Toma de Decisiones SIRTOD [http://iinei.inei.gob.pe/iinei/SIRTOD/]
+#'
+#' @docType data
+#' @keywords datasets
+#' @name aguapublica
+#' @usage data(aguapublica)
+#' @format Base de datos en formato XTS con la data de Sistema de Informacion Regional para la Toma de Decisiones SIRTOD 
+NULL
+
+#' Base de datos de Hogares Hogares con acceso a alcantarillado por Red Publica.
+#'
+#' Base de datos de Hogares con acceso a alcantarillado por Red Publica del Sistema de Informacion Regional para la Toma de Decisiones SIRTOD [http://iinei.inei.gob.pe/iinei/SIRTOD/]
+#'
+#' @docType data
+#' @keywords datasets
+#' @name alcantarillado
+#' @usage data(alcantarillado)
+#' @format Base de datos en formato XTS con la data de Sistema de Informacion Regional para la Toma de Decisiones SIRTOD 
+NULL
+
+#' Base de datos de Hogares Hogares con acceso a alumbrado eléctrico por Red Publica.
+#'
+#' Base de datos de Hogares con acceso a alumbrado eléctrico por Red Publica  del Sistema de Informacion Regional para la Toma de Decisiones SIRTOD [http://iinei.inei.gob.pe/iinei/SIRTOD/]
+#'
+#' @docType data
+#' @keywords datasets
+#' @name electrico
+#' @usage data(electrico)
+#' @format Base de datos en formato XTS con la data de Sistema de Informacion Regional para la Toma de Decisiones SIRTOD 
+NULL
+
+#' Base de datos de Informalidad por Departamento.
+#'
+#' Base de datos de Informalidad por Departamento  del Sistema de Informacion Regional para la Toma de Decisiones SIRTOD [http://iinei.inei.gob.pe/iinei/SIRTOD/]
+#'
+#' @docType data
+#' @keywords datasets
+#' @name informalidad
+#' @usage data(informalidad)
+#' @format Base de datos en formato XTS con la data de Sistema de Informacion Regional para la Toma de Decisiones SIRTOD 
+NULL
+
+#' Base de datos de Ocupacion por Departamento.
+#'
+#' Base de datos de Ocupacion por Departamento  del Sistema de Informacion Regional para la Toma de Decisiones SIRTOD [http://iinei.inei.gob.pe/iinei/SIRTOD/]
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ocupacion
+#' @usage data(ocupacion)
+#' @format Base de datos en formato XTS con la data de Sistema de Informacion Regional para la Toma de Decisiones SIRTOD 
+NULL
+
+#' Base de datos de PBI a precios constantes 2007 por Departamento.
+#'
+#' Base de datos de PBI a precios constantes 2007  del Sistema de Informacion Regional para la Toma de Decisiones SIRTOD [http://iinei.inei.gob.pe/iinei/SIRTOD/]
+#'
+#' @docType data
+#' @keywords datasets
+#' @name PBI2007
+#' @usage data(PBI2007)
+#' @format Base de datos en formato XTS con la data de Sistema de Informacion Regional para la Toma de Decisiones SIRTOD 
+NULL
+
+#' Base de datos de PEA (Poblacion Economicamente Activa) por Departamento.
+#'
+#' Base de datos de PEA (Poblacion Economicamente Activa) por Departamento  del Sistema de Informacion Regional para la Toma de Decisiones SIRTOD [http://iinei.inei.gob.pe/iinei/SIRTOD/]
+#'
+#' @docType data
+#' @keywords datasets
+#' @name PEA
+#' @usage data(PEA)
+#' @format Base de datos en formato XTS con la data de Sistema de Informacion Regional para la Toma de Decisiones SIRTOD 
+NULL
+
+#' Base de datos de Poblacion Estimada por Departamento.
+#'
+#' Base de datos de Poblacion Estimada por Departamento  del Sistema de Informacion Regional para la Toma de Decisiones SIRTOD [http://iinei.inei.gob.pe/iinei/SIRTOD/]
+#'
+#' @docType data
+#' @keywords datasets
+#' @name poblacionestimada
+#' @usage data(poblacionestimada)
+#' @format Base de datos en formato XTS con la data de Sistema de Informacion Regional para la Toma de Decisiones SIRTOD 
+NULL
+
+#' Base de datos de Pobreza por Departamento.
+#'
+#' Base de datos de Pobreza por Departamento  del Sistema de Informacion Regional para la Toma de Decisiones SIRTOD [http://iinei.inei.gob.pe/iinei/SIRTOD/]
+#'
+#' @docType data
+#' @keywords datasets
+#' @name pobreza
+#' @usage data(pobreza)
+#' @format Base de datos en formato XTS con la data de Sistema de Informacion Regional para la Toma de Decisiones SIRTOD 
+NULL
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# INEI
-Proyecto de BEST con la finalidad de trabajar data del INEI de forma sencilla.
